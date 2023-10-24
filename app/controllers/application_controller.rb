e07004086f5b15e53945f797fbcb75f8bc5d64f6
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-10-24 15:33:16 +0000

    [Finish] 4-7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     about_path
   end
+  
+  def after_sign_out_path_for(resource)
+    about_path
+  end
 
   protected
 
