c71040412ac47d598460c3e4e7aae69ccba33cfd
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-10-26 12:07:53 +0000

    [Finish] 4-10

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,6 +1,13 @@
 class PostImagesController < ApplicationController
   def new
-    @post_image = Post_Image.new
+    @post_image = PostImage.new
+  end
+  
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
   end
 
   def index
@@ -8,4 +15,12 @@ class PostImagesController < ApplicationController
 
   def show
   end
+  
+  private
+  
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+    
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   #get 'post_images/new'
   #get 'post_images/index'
   #get 'post_images/show'
-  resources :post_images, only: [:new, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show]
   
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
