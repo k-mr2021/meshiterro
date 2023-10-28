8e9280896caa2133b04d2c3b414a6258dab319ab
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-10-28 13:35:46 +0000

    [Finish] 4-16

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   
   #get 'users/show'
   #get 'users/edit'
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
