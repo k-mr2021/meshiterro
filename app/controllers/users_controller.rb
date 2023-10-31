4373058433294838d306ae6e2d147597478ff3e6
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-10-31 15:33:38 +0000

    [Finish] 4-26

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,16 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
-    # 他ユーザーからのアクセス制限
-    user = User.find(params[:id])
-    unless user.id == current_user.id
-      redirect_to post_iamges_path
-    end
-    
     @user = User.find(params[:id])
   end
   
   def update
-     # 他ユーザーからのアクセス制限
-    user = User.find(params[:id])
-    unless user.id == current_user.id
-      redirect_to post_iamges_path
-    end
-    
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
@@ -30,6 +20,14 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+  
+   # 他ユーザーからのアクセス制限
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
   end
   
 end
