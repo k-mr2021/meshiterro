003e8b8a37d1589b834f4669af01c0356dbe9b04
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-10-31 15:44:26 +0000

    [Start] 4-27

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -38,6 +38,7 @@ Devise.setup do |config|
   # available as additional gems.
   require 'devise/orm/active_record'
 
+  # 任意の認証方法に変更できる
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
   # just :email. You can configure it to use [:username, :subdomain], so for
@@ -46,7 +47,7 @@ Devise.setup do |config|
   # session. If you need permissions, you should implement that in a before filter.
   # You can also supply a hash where the value is a boolean determining whether
   # or not authentication should be aborted when the value is not present.
-  # config.authentication_keys = [:email]
+  config.authentication_keys = [:name]
 
   # Configure parameters from the request object used for authentication. Each entry
   # given should be a request method and it will automatically be passed to the
